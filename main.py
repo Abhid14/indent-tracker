54ca7875c5d93b1ad47ff181083d974d4386881c
Abhishek Das <abhid14@outlook.com>
2022-09-09 21:09:32 +0530

    t7

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def reload():
 @app.get("/dump")
 async def dump():
     #os.system("DROP DATABASE databasename;")
-    return {"status": "dump ok donesdadaaaa"}
+    return {"status": "dump ok done"}
 
 
 @app.get("/restart")
