35b768813f9c8acb45116522f6e9349ac7f9d51c
Abhishek Das <abhid14@outlook.com>
2022-09-12 00:59:16 +0530

    random push

--- k.py
+++ k.py
@@ -1,2 +1,8 @@
-import subprocess
-subprocess.Popen('git pull', shell=True)
+import mysql.connector
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="lab22",
+)
+
+print(mydb)
